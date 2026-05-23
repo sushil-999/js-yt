b0a02ab84bf77638bd4d61fe756b71a144e4321d
sushil-999 <sherchansushil37@gmail.com>
2026-05-23 16:53:04 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score = true
+
+// '99'=> 99
+// "99abc" => NaN 
+//null=> 0
+// true=> 1, false=> 0
+//console.log(typeof score)
+
+//let valueInNumber =  Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true, 0=> false
+//""=> false
+// "sushil"=> true
+
+let someNumber = 999
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
