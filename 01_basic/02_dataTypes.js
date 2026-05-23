a8d4c0704457b917e8a8d54b4fe40f2d6fc1e23b
sushil-999 <sherchansushil37@gmail.com>
2026-05-23 16:33:39 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as newer version
+
+//alert("hello")// we are using nodejs not browser
+
+console.log(3+3)
+let name = 'sushil'
+console.log("sushil")
+/* PRIMITIVE DATATYPES
+// number => 2 to power 53
+//bigint => for big numbers
+// string => ""
+//boolean => true/false
+//null => stand alone value (empty)
+// undefined  => value not assigned
+//symbol => unique 
+
+// object*/
+
+console.log(typeof name);
