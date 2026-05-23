f2538cfa94cca6df5c196911f7e4ccc050b2bb3c
sushil-999 <sherchansushil37@gmail.com>
2026-05-23 04:23:24 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 999
+let accountName = "sushil"
+var accountPlace = "bhw"
+
+//accountId = 100
+
+accountName = "samir" 
+accountPlace = "mandir"
+accountEmail  = "999@mail.com"
+
+/*  as much as possible dont use  "var" due to the issue with block scope and functional scope */
+console.table([accountId, accountName,  accountPlace, accountEmail])
+
+//console.log(accountName);
